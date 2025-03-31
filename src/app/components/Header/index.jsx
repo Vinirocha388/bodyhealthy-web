47b5c5f65a6cca42802e424ddb136de6f0ba46a4
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-03-31 15:54:44 -0300

    feat: completion of home responsiveness and creation of mobile menu

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -1,14 +1,30 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./header.module.css";
 import Link from "next/link";
 import { FaUserCircle } from "react-icons/fa";
 import { MdOutlineMenu } from "react-icons/md";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
-                <MdOutlineMenu className={styles.menuIcon} size={60} color="white" />
-                <a className={styles.home} href={"/"}> <h1 className={styles.logoText}>Body & Health</h1></a>
+                <MdOutlineMenu 
+                    className={styles.menuIcon} 
+                    size={60} 
+                    color="white" 
+                    onClick={toggleMenu} 
+                />
+                <a className={styles.home} href={"/"}> 
+                    <h1 className={styles.logoText}>Body & Health</h1>
+                </a>
                 <FaUserCircle className={styles.userIcon} size={50} color="white" />
             </div>
             <nav className={styles.mainNav}>
@@ -21,8 +37,21 @@ const Header = () => {
                     <li><Link href={"/contato"} className={styles.navLink}>Contato</Link></li>
                 </ul>
             </nav>
+            {menuOpen && (
+    <nav className={`${styles.dropdownMenu} ${menuOpen ? styles.open : ""}`}>
+        <ul>
+            <li><Link href={"/"} className={styles.navLink}>Início</Link></li>
+            <li><Link href={"/servicos"} className={styles.navLink}>Serviços</Link></li>
+            <li><Link href={"/dicas"} className={styles.navLink}>Dicas</Link></li>
+            <li><Link href={"/treinos"} className={styles.navLink}>Treinos</Link></li>
+            <li><Link href={"/login"} className={styles.navLink}>Login</Link></li>
+            <li><Link href={"/contato"} className={styles.navLink}>Contato</Link></li>
+        </ul>
+    </nav>
+)}
+
         </header>
-    )
-}
+    );
+};
 
 export default Header;
