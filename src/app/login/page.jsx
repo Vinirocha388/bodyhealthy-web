411fad370ad36371ec76b53bb09500d0d8e6e124
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-04-01 14:55:42 -0300

    feat: add login and servicos page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,17 +1,42 @@
-
-import styles from './login.module.css';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import styles from "./login.module.css";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function login() {
-    return (
-        <div className={styles.container}>
-            <Header />
-            <main>
-                <h1>Login</h1>
-                <p>conteúdo do login estará aqui</p>
-            </main>
-            <Footer />
+  return (
+    <div className={styles.container}>
+      <Header />
+      <main>
+        <h1 className={styles.logoText}>Body & Health</h1>
+        <div className={styles.inputDiv}>
+          <div className={styles.textBox}>
+            <input
+              type="text"
+              placeholder="endereço de e-mail ou nome de usuário"
+              className={styles.textBoxText}
+            />
+          </div>
+          <div className={styles.textBox}>
+            <input
+              type="text"
+              placeholder="senha"
+              className={styles.textBoxText}
+            />
+          </div>
+          <div className={styles.loginTextDiv}>
+            <div className={styles.loginTextCollumn}>
+              <p className={styles.loginText}>Esqueceu minha senha</p>
+              <p className={styles.loginText}>
+                Não tem uma conta? Se cadastre no Body & Health
+              </p>
+            </div>
+            <div className={styles.buttonDiv}>
+              <button className={styles.button}>Entrar</button>
+            </div>
+          </div>
         </div>
-    );
+      </main>
+      <Footer />
+    </div>
+  );
 }
