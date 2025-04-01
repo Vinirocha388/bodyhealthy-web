2efd27b3211ec62dd6027b349c01de805c417b53
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-04-01 14:56:24 -0300

    feat: add contact page

--- src/app/contato/page.jsx
+++ src/app/contato/page.jsx
@@ -1,16 +1,33 @@
-import Footer from '../components/Footer';
-import styles from './contato.module.css';
-import Header from '../components/Header';
+import Footer from "../components/Footer";
+import styles from "./contato.module.css";
+import Header from "../components/Header";
 
 export default function contato() {
-    return (
-        <div className={styles.contato}>
-            <Header />
-            <main>
-                <h1>Contato</h1>
-                <p>Essa é a página de contato</p>
-            </main>
-            <Footer />
+return (
+    <div className={styles.container}>
+    <Header />
+    <div className={styles.contatoImagem}>
+        <h1 className={styles.contatoTitle}>Contato</h1>
+        <div className={styles.contatoContainer}>
+        <div className={styles.contatoContainerDiv}>
+
+            <div className={styles.contatoNome}>
+                <h2>Seu nome</h2>
+                <input name="name" type="name" placeholder=" Nome:" className={styles.contatoNameInput} required/>
+            </div>
+                <div className={styles.contatoEmail}>
+                <h2>Seu email</h2>
+                <input name="email"type="email" placeholder=" Email:" className={styles.contatoEmailInput} required/>
+            </div>
+            <div className={styles.contatoMensagem}>
+                <h2>Mensagem</h2>
+                <textarea name="message" placeholder=" Sua mensagem aqui:"  className={styles.contatoMensagemInput} required></textarea>
+            </div>
+
         </div>
-    );
+        </div>
+    </div>
+    <Footer />
+    </div>
+);
 }
