079e47c562ffa4a972d4f7b930761fe2d9dac214
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-04-22 09:29:03 -0300

    refactor Services., add components

--- src/app/postagens/page.jsx
+++ src/app/postagens/page.jsx
@@ -2,7 +2,7 @@ import { FaCommentMedical } from "react-icons/fa";
 
 import styles from "./postagens.module.css"
 import Header from "../components/Header";
-import Post from "../components/posts";
+import Post from "../components/Posts";
 import Footer from "../components/Footer";
 
 export default function Home() {
