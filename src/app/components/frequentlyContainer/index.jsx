b4e461a3142be8b91f86bd7ed1d50cf3be48b2f8
vitor.argeri@aluno.senai.br <vitor.argeri@aluno.senai.br>
2025-04-29 15:58:16 -0300

    fix: frequentlyContainer and contato page

--- src/app/components/frequentlyContainer/index.jsx
+++ src/app/components/frequentlyContainer/index.jsx
@@ -1,12 +1,14 @@
 import styles from "./frequentlyContainer.module.css";
 
-const FrequentlyContainer = ({question, answer}) => {
+const FrequentlyContainer = ({ question, answer }) => {
     return (
-        <details className={styles.frequentlyQuestionsContainer}>
-            <summary className={styles.frequentlyQuestionsText}>{question}</summary>
-            <p className={styles.frequentlyQuestionsAnswer}>{answer}</p>
+        <details className={styles.questionItem}>
+            <summary className={styles.questionSummary}>
+                <strong>{question}</strong>
+            </summary>
+            <p className={styles.questionAnswer}>{answer}</p>
         </details>
-    )
-}
+    );
+};
 
 export default FrequentlyContainer;
