4c95c15233c5892c92f62f2539609edb5b681332
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2025-06-05 15:29:08 -0300

    feat: create components cardExercicios & Exercicion with paginasTreinos

--- src/app/components/Exercicios/index.jsx
+++ src/app/components/Exercicios/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./exercicios.module.css";
 
-const Exercicios = ({ image, title, repetitions, text }) => {
+const Exercicios = ({ image, title, workout, series, repetition, text, topic, number }) => {
     return (
         <div className={styles.workoutContainer}>
 
@@ -10,13 +10,49 @@ const Exercicios = ({ image, title, repetitions, text }) => {
             </div>
 
             <div className={styles.workoutInfoContainer}>
-                <h2 className={styles.workoutTitle}>{title}</h2>
-                <p className={styles.workoutRepetitions}>{repetitions}</p>
+                <h2 className={styles.workoutTitle}><span className={styles.topic}>{topic}</span> {title}</h2>
+                <p className={styles.workout}>{workout}</p>
+                <p className={styles.workoutSeries}>{series}</p>
+
+                    <select
+                        name={number}
+                        className={styles.workoutSelectSeries}
+                        required
+                  >
+                    <option value="" disabled>
+                      Quantidade de Séries:
+                    </option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>    
+                  </select>
+
+                  x
+                  
+                    <select
+                        name={number}
+                        className={styles.workoutSelectRepetition}
+                        required
+                  >
+                    <option value="" disabled className={styles.option}>
+                      Quantidade de Repetições:
+                    </option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                  </select>
+
+
             </div>
 
             <div className={styles.workoutTextContainer}>
-                <p className={styles.workoutText}>{text}rrr</p>
+                <div className={styles.workoutTextContent}>
+                <p className={styles.workoutText}>{text}</p>
+                </div>
             </div>
+
         </div>
     );
 };
--- src/app/paginasTreinos/page.jsx
+++ src/app/paginasTreinos/page.jsx
@@ -10,7 +10,7 @@ import CardExercicios from "../components/CardExercicios";
 import Exercicios from "../components/Exercicios";
 
 export default function PaginasTreinos() {
-  return (
+  return ( 
     <div className={styles.container}>
       <Header />
       <main>
@@ -26,13 +26,28 @@ export default function PaginasTreinos() {
         >
           <Exercicios
             image="https://treinomestre.com.br/wp-content/uploads/2017/08/supino-fechado-cp.jpg"
-            title="Nome: Supino Reto"
+            topic="Nome:"
+            title="Supino Reto"
+            workout="Volume de Treinos:"
+            series="Séries e Repetições"
+            number="4x10"
+            text="É um dos exercícios mais importantes para quem busca fortalecer e aumentar a massa muscular na região do peito. "
+            
+          />
+          <Exercicios
+            image="https://grandeatleta.com.br/blog/wp-content/uploads/2022/07/supino-inclinado-com-halteres-.jpg"
+            topic="Nome:"
+            title="Supino Inclinado"
+            workout="Volume de Treinos:"
+            series="Séries e Repetições:"
+            text="É um exercício que ativa, principalmente, os músculos do peitoral, mas também envolve os tríceps e deltoides."
+            
           />
         </CardExercicios>
+
 
       </main>
       <Footer />
     </div>
   );
 }
-
